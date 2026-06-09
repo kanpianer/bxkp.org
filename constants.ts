8958f2fd37e0096c5f9f7945a0aa4527ff88a0ce
kanpianer <225342149+kanpianer@users.noreply.github.com>
2026-06-09 19:57:20 -0400

    Add new resource '低调影视' and update others

--- constants.ts
+++ constants.ts
@@ -7,6 +7,7 @@ export const SITES: Site[] = [
     { name: "雪落影视", main_url: "https://v.xl01.eu.cc/", backup_url: "https://v.xl01.cc.ua/", tags: ["在线", "BT"] },
     { name: "七味", main_url: "https://www.qmp4.com", backup_url: "https://www.qn63.com/", tags: ["网盘", "BT"] },
     { name: "GoPlay/中文", main_url: "https://goplay.su/", backup_url: "https://discord.com/invite/yY2P3DQR8S", tags: ["在线"] },
+    { name: "低调影视", main_url: "https://ddys.app/", backup_url: "https://ddys.ai/", tags: ["在线"] },
     
     { name: "Cinegram", main_url:"https://cinegram.tv/home", backup_url:"cinegramofficial@proton.me", tags:["在线"]},
     { name: "EE3", main_url: "https://ee3.me/", backup_url: "https://rips.cc/", tags: ["在线"] },    
@@ -20,22 +21,22 @@ export const SITES: Site[] = [
   /*  { name: "BrocoFlix", main_url:"https://brocoflix.xyz/", backup_url:"https://rentry.co/KipsSites", tags:["在线"]}, */
   /*  { name: "Cinemaos", main_url:"https://cinemaos.live/", backup_url:"https://cinemaos.tech/", tags:["在线"]}, */
   /*  { name: "Aether", main_url: "https://aether.mom", backup_url: "https://aether.bar/", tags: ["在线", ""] }, */
+  /*  { name: "两个BT", main_url: "https://www.bttwo.me", backup_url: "https://bttwo.vip/", tags: ["在线", ""] }, */
+  /*  { name: "混合盘", main_url: "https://hunhepan.com/search", backup_url: "https://hhp.panso.me", tags: ["网盘"] }, */
     
     { name: "趣盘 Alist", main_url: "https://pan.mediy.cn", backup_url: "", tags: ["在线", "网盘"] },
-    { name: "七米蓝 Alist", main_url: "https://al.chirmyram.com/", backup_url: "", tags: ["在线", "网盘"] },
-    { name: "两个BT", main_url: "https://www.bttwo.me", backup_url: "https://bttwo.vip/", tags: ["在线", ""] },
+    { name: "七米蓝 Openlist", main_url: "https://al.chirmyram.com/", backup_url: "", tags: ["在线", "网盘"] },
     { name: "海搜", main_url: "https://haisou.cc/", backup_url: "", tags: ["网盘"] },
     { name: "Pomo/中文", main_url: "https://pomo.mom/", backup_url: "", tags: ["BT"] },
+    { name: "爱盼", main_url: "https://www.aipan.me", backup_url: "", tags: ["网盘"] },
     { name: "玩偶哥哥", main_url: "https://www.wogg.net/", backup_url: "", tags: ["网盘"] },
-    { name: "南方公园全集", main_url: "https://sp.tearemix.com/#s27", backup_url: "", tags: ["网盘"] },
     { name: "网盘俱乐部", main_url: "https://pan.club/", backup_url: "", tags: ["网盘"] },
-    { name: "夸盘资源网", main_url: "https://www.ppat.top/", backup_url: "", tags: ["网盘"] },
+    { name: "盘搜PanSou", main_url: "https://so.252035.xyz", backup_url: "", tags: ["网盘"] },
     { name: "老K", main_url: "https://docs.qq.com/doc/DTk5EaVdWY2R6ZnBk", backup_url: "", tags: ["网盘"] },
     { name: "4k指南[原盘]", main_url: "https://4kzn.com", backup_url: "", tags: ["网盘"] },
-    { name: "混合盘", main_url: "https://hunhepan.com/search", backup_url: "https://hhp.panso.me", tags: ["网盘"] },
     { name: "网盘资源避难所", main_url: "https://www.daiguaji.com/index.php", backup_url: "", tags: ["网盘"] },
-    { name: "爱盼", main_url: "https://www.aipan.me", backup_url: "", tags: ["网盘"] },
     { name: "阿里云资源", main_url: "https://slowread.net/", backup_url: "", tags: ["网盘"] },
+    { name: "南方公园全集", main_url: "https://sp.tearemix.com/#s27", backup_url: "", tags: ["网盘"] },
     { name: "ShowBox", main_url: "https://www.showbox.media/", backup_url: "", tags: ["网盘"] },
     
     { name: "BT之家", main_url: "https://1lou.one/", backup_url: "https://1lou.cc/", tags: ["BT", "网盘"] },
